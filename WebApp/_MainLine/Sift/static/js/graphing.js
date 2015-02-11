32c0d882c4f95b169f9c0fb0f1f21e5975b9d160
Maxime Goovaerts <mgoovaer@gmail.com>
2015-02-11 14:03:44 -0500

    added Google Line Chart

--- WebApp/_MainLine/Sift/static/js/graphing.js
+++ WebApp/_MainLine/Sift/static/js/graphing.js
@@ -1,42 +1,6 @@
 /**
  * Created by MaxGoovaerts on 2/8/2015.
  */
-function drawGraph() {
-    var ctx = document.getElementById("lineChart").getContext("2d");
-    var data = {
-        labels: ["January", "February", "March", "April", "May", "June", "July"],
-        datasets: [
-            {
-                label: "My First dataset",
-                fillColor: "rgba(220,220,220,0.2)",
-                strokeColor: "rgba(220,220,220,1)",
-                pointColor: "rgba(220,220,220,1)",
-                pointStrokeColor: "#fff",
-                pointHighlightFill: "#fff",
-                pointHighlightStroke: "rgba(220,220,220,1)",
-                data: [65, 59, 80, 81, 56, 55, 40]
-            },
-            {
-                label: "My Second dataset",
-                fillColor: "rgba(151,187,205,0.2)",
-                strokeColor: "rgba(151,187,205,1)",
-                pointColor: "rgba(151,187,205,1)",
-                pointStrokeColor: "#fff",
-                pointHighlightFill: "#fff",
-                pointHighlightStroke: "rgba(151,187,205,1)",
-                data: [28, 48, 40, 19, 86, 27, 90]
-            }
-        ]
-    };
-    var myLineChart = new Chart(ctx).Line(data);
-    document.getElementById("lineLegend").innerHTML = myLineChart.generateLegend();
-}
-
-    function drawPieChart(data) {
-        var ctx = document.getElementById("pieChart").getContext("2d");
-        var myPieChart = new Chart(ctx).Pie(data);
-    }
-
     function drawGooglePieChart(array) {
         var data = google.visualization.arrayToDataTable(array);
 
@@ -53,3 +17,48 @@ function drawGraph() {
         var chart = new google.visualization.PieChart(document.getElementById('piechart_3d'));
         chart.draw(data, options);
     }
+
+    function drawTimeLineChart(count, year, month, day) {
+        var data = new google.visualization.DataTable();
+
+        data.addColumn('date', 'Date');
+        data.addColumn('number', 'Posts');
+        for(i=0; i<count.length; i++)
+        {
+            data.addRows([
+                [new Date(year[i], month[i], day[i]), count[i]]
+            ])
+        }
+
+
+        var options = {
+          title: 'Number of Forum Posts over Time',
+          displayAnnotations: true
+        };
+
+        var chart = new google.visualization.AnnotationChart(document.getElementById('linechart_annotation'));
+        chart.draw(data, options);
+    }
+
+//function drawDashboard(mod, post, categoryid, year, month, day) {
+//        var data = new google.visualization.DataTable();
+//
+//        data.addColumn('date', 'Date');
+//        data.addColumn('number', 'Posts');
+//        for(i=0; i<count.length; i++)
+//        {
+//            data.addRows([
+//                [new Date(year[i], month[i], day[i]), count[i]]
+//            ])
+//        }
+//
+//
+//        var options = {
+//          title: 'Number of Forum Posts over Time',
+//          displayAnnotations: true
+//        };
+//
+//        var chart = new google.visualization.AnnotationChart(document.getElementById('linechart_annotation'));
+//        chart.draw(data, options);
+//    }
+
--- WebApp/_MainLine/Sift/views.py
+++ WebApp/_MainLine/Sift/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from Sift.models import Cluster, Post
+from django.db.models import Count
 import json
+import datetime
 
 def general(request):
     headline = "General Analytics"
@@ -17,17 +19,30 @@ def details(request, cluster_id):
     trendingClusters = Cluster.objects.filter(ispinned=0)
     pinnedClusters = Cluster.objects.filter(ispinned=1)
 
+    # for s in Post:
+    #     dashCategory.append
 
-    data_dict = [{'value': Post.objects.filter(forumid=2).count(), 'label': 'red', 'color': '#F7464A'}
-        , {'value': Post.objects.filter(forumid=3).count(), 'label': 'blue', 'color': '#468FBD'}]
-    data = json.dumps(data_dict)
 
-    pie_data = json.dumps([['Forum ID', 'Number of Posts'],
+
+    count = Post.objects.values('creationdate').annotate(postCount = Count('threadid'))
+    dateCount = list()
+    dateY = list()
+    dateM = list()
+    dateD = list()
+    for s in count:
+        dateY.append(s["creationdate"].year)
+        dateM.append(s["creationdate"].month)
+        dateD.append(s["creationdate"].day)
+        dateCount.append(s["postCount"])
+
+
+    pieData = ([['Forum ID', 'Number of Posts'],
                         ['2', Post.objects.filter(forumid=2).count()],
-                        ['2', Post.objects.filter(forumid=3).count()]])
+                        ['3', Post.objects.filter(forumid=3).count()]])
 
     context = {'pinnedClusters': pinnedClusters, 'trendingClusters': trendingClusters, "headline": headline,
-               'cluster': cluster, 'data': data, 'pie_data': pie_data}
+               'cluster': cluster, 'pieData': pieData, 'lineDataCount': dateCount, 'lineDataDateY': dateY,
+               'lineDataDateM': dateM, 'lineDataDateD': dateD,}
     return render(request, 'details.html', context)
 
 
@@ -38,12 +53,3 @@ def settings(request):
 
     context = {'pinnedClusters': pinnedClusters, 'trendingClusters': trendingClusters, "headline": headline}
     return render(request, 'settings.html', context)
-
-
-class piechartData:
-
-    def __init__(self, value):
-        self.value = value
-
-    def __init__(self, label):
-        self.label = label
