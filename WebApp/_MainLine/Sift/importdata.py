3ae2f9903cd87dd688a190ed4bf9dc2dfec5aa1f
Maxime Goovaerts <mgoovaer@gmail.com>
2015-02-18 01:13:33 -0500

    updated import data script

--- WebApp/_MainLine/Sift/importdata.py
+++ WebApp/_MainLine/Sift/importdata.py
@@ -23,12 +23,12 @@ if not settings.configured:
         }
     )
 
-from models import *
+# from models import *
 import logging
 from time import time
 from datetime import datetime
 
-DATA_FILES = ['data/expSForums' + str(i) + '.cvs' for i in range(1,11)]
+DATA_FILES = ['data/expSForums' + str(i) + '.cvs' for i in range(2,11)]
 
 def init_db():
     conn = pymysql.connect(host='sellerforums.cqtoghgwmxut.us-west-2.rds.amazonaws.com', port=3306, user='teamamzn', passwd='TeamAmazon2015!', db='sellerforums')
@@ -49,12 +49,15 @@ def get_post_count(conn):
 
 def commitPost(post, conn):
         cur = conn.cursor()
-        query = "INSERT INTO posts (threadId, messageId, forumId, userId, categoryId, subject, body, postedByModerator," \
+        query = "INSERT INTO data (threadId, messageId, forumId, userId, categoryId, subject, body, postedByModerator," \
                 " resolutionState, helpfulAnswer, correctAnswer, userName, userPoints, creationDate, modificationDate, locale) VALUES "
         query += "(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
-        cur.execute(query, (post[0], post[1], post[2], post[3], post[4], post[5], post[6], post[7], post[8], post[9], post[10], post[11], post[12], post[13], post[14], post[15]))
+        # posts need to be encoded to avoid weird character breaking errors
+        cur.execute(query, (post[0], post[1], post[2], post[3], post[4], post[5].encode('ascii', 'replace'), post[6].encode('ascii', 'replace'),
+                            post[7], post[8], post[9], post[10], post[11], post[12], post[13], post[14], post[15]))
         conn.commit()
 
+# this function converts the provided time format to the mysql date format necessary for storing properly
 def convertTime(input):
     date_obj = datetime.strptime(input, '%m/%d/%y')
     return date_obj.strftime('%Y/%m/%d')
@@ -71,26 +74,25 @@ def main():
     print("Importing Data")
     t0 = time()
     i = 0
-    j = 10
+    j = 300
     for f in DATA_FILES:
-        p = 0
+        p = 60000
         print("Reading Data File: " + str(f))
         with open(f, encoding="utf-8", errors="surrogateescape") as data_file:
             while True:
                 parse = lambda i: i if '=' in i else '=\n'
-                data = [parse(data_file.readline()).split('=', 2)[1].rstrip().encode('ascii', 'replace')
+                data = [parse(data_file.readline()).split('=', 1)[1].rstrip()
                         for i in range(17)][:-1]
-                data[13] = convertTime(data[13].decode('utf-8'))
-                data[14] = convertTime(data[14].decode('utf-8'))
                 if data[0]:
+                    data[13] = convertTime(data[13])
+                    data[14] = convertTime(data[14])
                     i += 1
                     commitPost(data, conn)
                 else:
                     break
                 if (i-j == 0):
-                    p += 5
                     j += 300
-                    print("% of file: " + str(p) + " post: " + str(i) + " time: " + str(round(time()-t0, 2)))
+                    print("% of file: " + str(round(p/600.0, 3)) + " post: " + str(i) + " time: " + str(round(time()-t0, 2)))
 
         print("Successfully read " + str(i) + " posts")
     print("Data Imported:")
