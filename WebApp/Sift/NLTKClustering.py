dc68536392b29145611ed57a0278bd4a137d4ef6
Maxime Goovaerts <mgoovaer@gmail.com>
2015-03-04 09:34:34 -0500

    date change

--- WebApp/Sift/NLTKClustering.py
+++ WebApp/Sift/NLTKClustering.py
@@ -363,7 +363,7 @@ def main():
 
     dataset = ClusterData(
 
-    Post.objects.filter(creationdate__range=("2012-01-01", "2015-01-01")))
+    Post.objects.filter(creationdate__range=("2013-01-01", "2015-01-01")))
 
 
     cluster_posts(dataset, t0, NUM_CLUSTERS, MAX_FEATURES)
