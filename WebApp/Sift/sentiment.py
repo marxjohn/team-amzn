3b2e73e1ca6255f96dcdc0d9d6feb0f3c62d2c15
Maxime Goovaerts <mgoovaer@gmail.com>
2015-04-03 16:18:52 -0400

    lazy_sentiment updates

--- WebApp/Sift/sentiment.py
+++ WebApp/Sift/sentiment.py
@@ -47,40 +47,45 @@ def lazy_sentiment(start_date, end_date):
 
     suc = 0
     skip = 0
+    goto = 47518
+    i = 0
     print("lazy sentiment time!")
     for post in dataset:
-        sentimentobj = Sentiment.objects.filter(post_id=post.post_id)
-        if sentimentobj.__len__() is 0:
-            try:
-                body = html.document_fromstring(post.body).text_content()
-            except:
-                body = ""
-            payload = {'text': body[:80000]}
-            r = requests.post("http://text-processing.com/api/sentiment/", data=payload)
-            try:
-                print(payload)
-                print(r.json())
-                senti = Sentiment(post_id=post, prob_negative=r.json()['probability']['neg'],
-                                  prob_neutral=r.json()['probability']['neutral'], prob_positive=r.json()['probability']['pos'],
-                                  label=r.json()['label'])
-                senti.save()
-                suc += 1
-            except:
-                print("broke :(")
-                print(r)
-                if str(r) == "<Response [400]>":
-                    print("bad text")
-                    senti = Sentiment(post_id=post, prob_negative=0,
-                                  prob_neutral=0, prob_positive=0,
-                                  label="none")
+        i += 1
+        print(str(i))
+        if i > goto:
+            sentimentobj = Sentiment.objects.filter(post_id=post.post_id)
+            if sentimentobj.__len__() is 0:
+                try:
+                    body = html.document_fromstring(post.body).text_content()
+                except:
+                    body = ""
+                payload = {'text': body[:80000]}
+                r = requests.post("http://text-processing.com/api/sentiment/", data=payload)
+                try:
+                    print(payload)
+                    print(r.json())
+                    senti = Sentiment(post_id=post, prob_negative=r.json()['probability']['neg'],
+                                      prob_neutral=r.json()['probability']['neutral'], prob_positive=r.json()['probability']['pos'],
+                                      label=r.json()['label'])
                     senti.save()
-                else:
-                    print("reset ip and launch again")
-                    break
+                    suc += 1
+                except:
+                    print("broke :(")
+                    print(r)
+                    if str(r) == "<Response [400]>":
+                        print("bad text")
+                        senti = Sentiment(post_id=post, prob_negative=0,
+                                      prob_neutral=0, prob_positive=0,
+                                      label="none")
+                        senti.save()
+                    else:
+                        print("reset ip and launch again")
+                        break
 
-        else:
-            skip += 1
-            print(str(skip) + " skipped")
+            else:
+                skip += 1
+                print(str(skip) + " skipped")
 
     print("Successfully sentimented: " + str(suc) + " posts")
     print("Skipped over: " + str(skip) + " posts")
