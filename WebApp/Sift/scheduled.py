614afa03645e1094e9b908893be74324e22ba731
Kangzhuang <zhengkan@msu.edu>
2015-04-06 17:15:10 -0400

    Revert "sns topic selected notification(topic transfer not finish)"
    This reverts commit 82f599549f3f5f7170f9ec007c326e1c2f8151ea.
    
    Conflicts:
    	WebApp/Sift/Notification.py
    	WebApp/Sift/scheduled.py
    	WebApp/Sift/templates/notifications.html
    	WebApp/Sift/views.py

--- WebApp/Sift/scheduled.py
+++ WebApp/Sift/scheduled.py
@@ -1,17 +1,37 @@
+from __future__ import absolute_import
+
 from datetime import date
 from datetime import datetime
-from Sift.models import *
-from Sift.clustering import run_diagnostic_clustering
-from Sift.classification import classify_on_date_range
-from Sift.scikit_utilities import create_cluster_data
-from Sift.models import Notification
-from Sift.Notification import *
+try:
+    from Sift.models import *
+    from Sift.clustering import run_diagnostic_clustering
+    from Sift.classification import classify_on_date_range
+    from Sift.scikit_utilities import create_cluster_data
+    from Sift.models import Notification
+    from Sift.Notification import *
+except:
+    from clustering import run_diagnostic_clustering
+    from classification import run_classification
+    from models import *
+    from models import Notification
+    from Notification import *
+
 from scikit_utilities import create_cluster_data
+
+
+
+
+
+try:
+    import pymysql
+    pymysql.install_as_MySQLdb()
+
+except:
+    pass
+
 import django
-from django.conf import settings
-import pymysql
-pymysql.install_as_MySQLdb()
 django.setup()
+from django.conf import settings
 if not settings.configured:
     settings.configure(
         DATABASES={'default': {
@@ -43,28 +63,38 @@ def find_min_and_max_date(c_list):
     return end_date, start_date
 
 
-def send_nightly_runner(email_text):
-    make_email_list = SESVerifyEmail()
-    email_list = make_email_list.make_verify_email_list()
+def send_nightly_runner( email_text ):
+    make_nightly_subscription = SNSNotification()
+    make_nightly_subscription.make_arn_list()
+    nightly_subscription = Sift.Notification.get_nightly_list()
 
-    if email_list is None:
+    if nightly_subscription == None:
         return False
     else:
-        topic = 'SIFT MSU Runner Notification'
-        send_message = SESMessage(
-            "siftmsu15@gmail.com", "siftmsu15@gmail.com", topic)
-        for i in range(1, len(email_list)):
-            send_message.add_cc_address(email_list[i])
-        # Create email contents with information
-        send_message.set_text(email_text)
-        send_message.send()
-        return True
+        make_nightly_subscription.set_topic_arn( 'NightlyRun' )
+        make_nightly_subscription.set_message( email_text )
+
+        make_nightly_subscription.publication()
+
+    # make_email_list = SESVerifyEmail()
+    # email_list = make_email_list.make_verify_email_list()
+    #
+    # if email_list == None:
+    #     return False
+    # else:
+    #     topic = 'SIFT MSU Runner Notification'
+    #     send_message = SESMessage("siftmsu15@gmail.com", "siftmsu15@gmail.com", topic)
+    #     for i in range(1, len(email_list)):
+    #         send_message.add_cc_address(email_list[i])
+    #     # Create email contents with information
+    #     send_message.set_text(email_text)
+    #     send_message.send()
+    #     return True
 
 
 def run_clustering(data, posts):
     end_date, start_date = find_min_and_max_date(posts)
-    s_score, s_inertia = run_diagnostic_clustering(
-        data, start_date, end_date, 1000, 5, .85, 20, 50, 150)
+    s_score, s_inertia = run_diagnostic_clustering(data, start_date, end_date, 1000, 5, .85, 20, 50, 150)
     return s_inertia, s_score
 
 
@@ -73,10 +103,8 @@ def main():
     end_date, start_date = find_min_and_max_date(test_list)
     #
 
-    train_list = Post.objects.filter(
-        cluster_id__isnull=False,
-        creation_date__range=('2014-01-01', '2014-01-12'))
-    # train_end_date, train_start_date = find_min_and_max_date(train_list)
+    train_list = Post.objects.filter(cluster_id__isnull=False, creation_date__range=('2014-01-01', '2014-01-12'))
+    #train_end_date, train_start_date = find_min_and_max_date(train_list)
     #
     test_data = create_cluster_data(test_list)
     train_data = create_cluster_data(train_list)
@@ -87,9 +115,10 @@ def main():
     data = create_cluster_data(posts)
     s_inertia, s_score = run_clustering(data, posts)
 
+
+
     # Some Magic here involving sending email alerts
-    text = "The status of the clusters are as follows: s_score: " + \
-        str(s_score) + ",  s_intertia: " + str(s_inertia)
+    text = "The status of the clusters are as follows: s_score: " + str(s_score) + ",  s_intertia: " + str(s_inertia)
     send_nightly_runner(text)
 
 if __name__ == '__main__':
