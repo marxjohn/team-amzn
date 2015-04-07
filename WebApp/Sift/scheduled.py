568a18eaa8cab2380cb3dc89b7cb169d91a2863a
lpritchett <pritch93@gmail.com>
2015-04-07 19:57:42 -0400

    fixed import error

--- WebApp/Sift/scheduled.py
+++ WebApp/Sift/scheduled.py
@@ -59,7 +59,7 @@ def find_min_and_max_date(c_list):
 def send_nightly_runner(email_text):
     make_nightly_subscription = SNSNotification()
     make_nightly_subscription.make_arn_list()
-    nightly_subscription = Sift.Notification.get_nightly_list()
+    nightly_subscription = get_nightly_list()
 
     if nightly_subscription == None:
         return False
@@ -69,21 +69,6 @@ def send_nightly_runner(email_text):
 
         make_nightly_subscription.publication()
         return True
-
-    # make_email_list = SESVerifyEmail()
-    # email_list = make_email_list.make_verify_email_list()
-    #
-    # if email_list == None:
-    #     return False
-    # else:
-    #     topic = 'SIFT MSU Runner Notification'
-    #     send_message = SESMessage("siftmsu15@gmail.com", "siftmsu15@gmail.com", topic)
-    #     for i in range(1, len(email_list)):
-    #         send_message.add_cc_address(email_list[i])
-    #     # Create email contents with information
-    #     send_message.set_text(email_text)
-    #     send_message.send()
-    #     return True
 
 
 def run_clustering(data, posts):
@@ -114,11 +99,10 @@ def main():
     data = create_cluster_data(posts)
     s_inertia, s_score = run_clustering(data, posts)
 
-
-
     # Some Magic here involving sending email alerts
     text = "The status of the clusters are as follows: s_score: " + str(s_score) + ",  s_intertia: " + str(s_inertia)
     send_nightly_runner(text)
+
 
 if __name__ == '__main__':
     main()
