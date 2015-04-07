1f66339271bbf8ed7308f3f497e055bb1d4e0278
lpritchett <pritch93@gmail.com>
2015-04-07 16:39:38 -0400

    Whoops even more tab silliness

--- WebApp/Sift/clustering.py
+++ WebApp/Sift/clustering.py
@@ -361,31 +361,31 @@ def upload_clusters(data_set, data_count, km, order_centroids,
         cursor.execute(query)
         cursor.close()
 
-        print("Counting Cluster Words")
-        # cwList = []
-        for x in range(1, num_clusters + 1):
-            c = Cluster.objects.get(clusterid=x)
-            cluster_words = []
+    print("Counting Cluster Words")
+    # cwList = []
+    for x in range(1, num_clusters + 1):
+        c = Cluster.objects.get(clusterid=x)
+        cluster_words = []
 
-            if len(order_centroids) < 100:
-                num_centroids = len(order_centroids)
-            else:
-                num_centroids = 100
+        if len(order_centroids) < 100:
+            num_centroids = len(order_centroids)
+        else:
+            num_centroids = 100
 
 
-            # Save top 100 cluster words
-            rank = 1;
-            for ind in order_centroids[x - 1, :num_centroids]:
-                count = len(
-                    Post.objects.filter(cluster=c,
-                                        stemmed_body__contains=terms[ind]))
+        # Save top 100 cluster words
+        rank = 1;
+        for ind in order_centroids[x - 1, :num_centroids]:
+            count = len(
+                Post.objects.filter(cluster=c,
+                                    stemmed_body__contains=terms[ind]))
 
-                cw = ClusterWord(word=terms[ind], clusterid=c, count=count, rank=rank)
-                cluster_words.append(cw)
-                rank += 1
+            cw = ClusterWord(word=terms[ind], clusterid=c, count=count, rank=rank)
+            cluster_words.append(cw)
+            rank += 1
 
-            cluster_words = sorted(cluster_words, key=attrgetter('count'), reverse=True)
-            ClusterWord.objects.bulk_create(cluster_words)
+        cluster_words = sorted(cluster_words, key=attrgetter('count'), reverse=True)
+        ClusterWord.objects.bulk_create(cluster_words)
 
     # ClusterWord.objects.bulk_create(cwList)
 
