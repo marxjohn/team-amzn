b5efe1853b6839b9b4de7de9356ead032b801d1d
Maxime Goovaerts <mgoovaer@gmail.com>
2015-04-08 17:53:04 -0400

    added emailing to diagnostic clustering script

--- WebApp/Sift/tasks.py
+++ WebApp/Sift/tasks.py
@@ -2,7 +2,7 @@ from celery import shared_task
 from time import time
 import Sift.clustering as clustering
 from Sift.models import Post
-from django.core.cache import cache
+from Sift.Notification import *
 import logging
 import datetime
 from django.core.cache import cache
@@ -13,6 +13,7 @@ __author__ = 'cse498'
 @shared_task
 def cluster_posts_with_input(start_date, end_date, num_clusters, max_features,
                              isMiniBatch, isAllPosts):
+    t0 = time()
     logging.basicConfig(level=logging.INFO,
                         format='%(asctime)s %(levelname)s %(message)s')
 
@@ -26,6 +27,21 @@ def cluster_posts_with_input(start_date, end_date, num_clusters, max_features,
     clustering.run_diagnostic_clustering(
         dataset, start_date, end_date, max_features,
         num_clusters, .85, 20, 50, 150)
+
+    # send email
+    email = SNSNotification()
+    email.make_arn_list()
+    email = get_nightly_list()
+
+    if email is not None:
+        email.set_topic_arn('Diagnostic Clustering')
+        email.set_message("Successfully completed Diagnostic Clustering in " + str(time() - t0) + " seconds! From "
+                          + str(start_date) + " to " + str(end_date) + " with "
+                          + str(num_clusters) + " clusters and " + str(max_features)
+                          + " features.")
+
+        email.publication()
+
     cache.clear()
 
 
