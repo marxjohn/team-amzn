8c67668d4147266af5d5180dc467d99feb3b2e3f
Benjamin Taylor <taylo886@msu.edu>
2015-04-15 16:08:40 -0400

    Fix errors in celery tasks

--- WebApp/Sift/tasks.py
+++ WebApp/Sift/tasks.py
@@ -6,9 +6,9 @@ from Sift._celery import app
 from django.core.cache import cache
 
 import Sift.clustering as clustering
-from Sift.models import Post
+from Sift.models import Post, Cluster
 from Sift.Notification import *
-from Sift.scikit_utilities import find_min_and_max_date, create_cluster_data
+from Sift.scikit_utilities import find_min_and_max_date, create_cluster_data, ClusterData
 from Sift.clustering import run_creation_clustering
 
 
@@ -16,7 +16,7 @@ __author__ = 'cse498'
 
 
 @app.task(bind=True)
-def cluster_posts_with_input(start_date, end_date, num_clusters, max_features,
+def cluster_posts_with_input(self, start_date, end_date, num_clusters, max_features,
                              isAllPosts):
     t0 = time()
     logging.basicConfig(level=logging.INFO,
@@ -26,8 +26,9 @@ def cluster_posts_with_input(start_date, end_date, num_clusters, max_features,
         start_date = "2000-01-01"
         end_date = datetime.date.today().strftime('%Y-%m-%d')
     self.update_state(state='FETCHING_POSTS')
-    dataset = clustering.ClusterData(
-        Post.objects.filter(creation_date__range=(start_date, end_date)))
+    dataset = ClusterData(
+        Post.objects.filter(creation_date__range=(start_date, end_date)),
+        Cluster.objects.all())
     self.update_state(state='RUNNING_CLUSTERING')
     clustering.run_diagnostic_clustering(
         dataset, start_date, end_date, max_features,
@@ -52,7 +53,7 @@ def cluster_posts_with_input(start_date, end_date, num_clusters, max_features,
 
 
 @app.task(bind=True)
-def create_new_clusters(num_clusters, max_features, max_df=.85, batch_size_ratio=20, init_size_ratio=50, n_init=150):
+def create_new_clusters(self, num_clusters, max_features, max_df=.85, batch_size_ratio=20, init_size_ratio=50, n_init=150):
     self.update_state(state='FETCHING_POSTS')
     posts = Post.objects.all()
     self.update_state(state='CREATING_CLUSTER_DATA')
