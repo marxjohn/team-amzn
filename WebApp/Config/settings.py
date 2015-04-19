8f41bfa2e24d805d1660c444b28d58974320c8cd
Maxime Goovaerts <mgoovaer@gmail.com>
2015-04-19 19:41:51 -0400

    set debug to false

--- WebApp/Config/settings.py
+++ WebApp/Config/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 SECRET_KEY = '3=v&4r_lweyxuis^kadmj@3+tety(u9sr1kkbp((o&9eh+prik'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 TEMPLATE_DEBUG = True
 
