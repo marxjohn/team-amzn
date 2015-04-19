7530eba697b3cab324442a8e5ef1e6ef02b60e91
lpritchett <pritch93@gmail.com>
2015-04-19 18:41:11 -0400

    Trying stuff

--- WebApp/Sift/pdf_generator.py
+++ WebApp/Sift/pdf_generator.py
@@ -14,7 +14,7 @@ def create_pdf(pdf_lines, cluster_run_id):
     story = []
     filename = str(cluster_run_id) + ".pdf"
     directory = '/pdf'
-    cur_dir = os.curdir
+    cur_dir = os.getcwd()
     local_file_path = os.path.join(directory, filename)
     full_file_path = cur_dir + local_file_path
     cluster_pdf = SimpleDocTemplate(full_file_path)
@@ -30,16 +30,3 @@ def create_pdf(pdf_lines, cluster_run_id):
     k.key = cluster_run_id
     k.set_metadata("Content-Type", "application/pdf")
     k.set_contents_from_filename(full_file_path)
-
-
-
-def main():
-    list = []
-    list.append("Hello")
-    create_pdf(list, 259)
-
-
-# Only run the main function if this code is called directly
-# Not if it's imported as a module
-if __name__ == '__main__':
-    main()
