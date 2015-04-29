222ec9c83f2cb804218dadee1cc1f98d8c61aef8
Maxime Goovaerts <mgoovaer@gmail.com>
2015-04-29 11:31:55 -0400

    debug to false

--- WebApp/Config/settings.py
+++ WebApp/Config/settings.py
@@ -20,9 +20,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 SECRET_KEY = '3=v&4r_lweyxuis^kadmj@3+tety(u9sr1kkbp((o&9eh+prik'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-TEMPLATE_DEBUG = True
+TEMPLATE_DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
